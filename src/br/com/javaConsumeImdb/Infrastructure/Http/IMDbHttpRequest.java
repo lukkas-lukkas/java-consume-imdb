85452b73fc30a9979d62228d439ead1d6f50c456
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-09-19 16:41:41 -0300

    add string return

--- src/br/com/javaConsumeImdb/Infrastructure/Http/IMDbHttpRequest.java
+++ src/br/com/javaConsumeImdb/Infrastructure/Http/IMDbHttpRequest.java
@@ -21,9 +21,37 @@ public class IMDbHttpRequest {
 	}
 
 	public String search() throws Exception {
-		return client
+		/*return client
 				.send(request, BodyHandlers.ofString())
-				.body();
+				.body();*/
+		return """
+				{
+				    "items": [
+				        {
+				            "id": "tt0111161",
+				            "rank": "1",
+				            "title": "The Shawshank Redemption",
+				            "fullTitle": "The Shawshank Redemption (1994)",
+				            "year": "1994",
+				            "image": "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_Ratio0.6716_AL_.jpg",
+				            "crew": "Frank Darabont (dir.), Tim Robbins, Morgan Freeman",
+				            "imDbRating": "9.2",
+				            "imDbRatingCount": "2641222"
+				        },
+				        {
+				            "id": "tt0068646",
+				            "rank": "2",
+				            "title": "The Godfather",
+				            "fullTitle": "The Godfather (1972)",
+				            "year": "1972",
+				            "image": "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_Ratio0.7015_AL_.jpg",
+				            "crew": "Francis Ford Coppola (dir.), Marlon Brando, Al Pacino",
+				            "imDbRating": "9.2",
+				            "imDbRatingCount": "1830893"
+				        }
+			       ]
+		       }
+			""";
 	}
 	
 	private URI getURI() {
